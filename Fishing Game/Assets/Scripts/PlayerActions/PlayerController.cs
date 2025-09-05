50e1aef812dbe8482a71e32b8af2161600a7765c
WyattGalloway <wyattgalloway123@aol.com>
2025-09-04 22:33:08 -0700

    Look Mom, a commit
    Boat movement adjustments.  Acceleration, decel, and better boat physics.

--- Fishing Game/Assets/Scripts/PlayerActions/PlayerController.cs
+++ Fishing Game/Assets/Scripts/PlayerActions/PlayerController.cs
@@ -3,14 +3,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //player input variables
     PlayerInput playerInput;
     InputAction movement;
-    [SerializeField] float turnSpeed = 30f;
-    [SerializeField] float moveSpeed;
-    [SerializeField] Rigidbody rb;
-
     private float forwardInput;
     private float turnInput;
+
+    [Header ("Boat Movements")]
+    [SerializeField] float turnSpeed;
+    [SerializeField] float currentSpeed;
+    [SerializeField] float maxMoveSpeed = 10.0f;
+    [SerializeField] float acceleration = 3.0f;
+    [SerializeField] float deceleration = 2.0f;
+    
+    [SerializeField] Rigidbody rb;
 
     void Start()
     {
@@ -36,14 +42,32 @@ public class PlayerController : MonoBehaviour
 
     void MovePlayer()
     {
-        float rotation = turnInput * turnSpeed * Time.fixedDeltaTime;
-        Quaternion turnOffset = Quaternion.Euler(0, rotation, 0);
-        rb.MoveRotation((rb.rotation * turnOffset));
+        //cap move speed
+        float targetSpeed = forwardInput * maxMoveSpeed;
 
-        Vector3 forwardMove = transform.forward * forwardInput * moveSpeed * Time.fixedDeltaTime;
+        //accelerate if holding forward or backward, else decelerate
+        if (Mathf.Abs(forwardInput) > 0.1f)
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deceleration * Time.fixedDeltaTime);
+        }
+
+        //Move forward/backward
+        Vector3 forwardMove = transform.forward * currentSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + forwardMove);
 
+        //scales turning based on moveSpeed, 0 when stopped and 1 when maxSpeed
+        float turnSpeedAmount = Mathf.InverseLerp(0, maxMoveSpeed, Mathf.Abs(currentSpeed));
+        //always turns at 30% speed
+        float effectiveTurnSpeed = turnSpeed * Mathf.Lerp(0.3f, 1f, turnSpeedAmount);
 
+        //Rotates the boat on y axis 
+        float rotation = turnInput * effectiveTurnSpeed * Time.fixedDeltaTime;
+        Quaternion turnOffset = Quaternion.Euler(0, rotation, 0);
+        rb.MoveRotation((rb.rotation * turnOffset));
     }
 }
 
